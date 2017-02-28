b2caee26febde8003fe70afab7f637b17f16f57e
User 01 <user01@teaching-F058.(none)>
2017-02-28 17:37:30 +0000

    Committee

---
+++ Python/basics.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+course = 'python'
+rating = 10
+print (course)
+print (rating)
+
+b=3
+c=4
+a=(b**2+c**2)**0.5
+print (a)
+
+print (type(a))
+print (type(b))
+print (type(c))
+
+int(a)
+print (type(a))
+print (str(a) + " squared equals " + str(b) + " squared plus " + str(c) + " squared.")
+
+
+
